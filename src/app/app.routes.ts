5badc9974907eb3d90a63f592e5dca7389daed15
Pedro Airton <pedroairton01@gmail.com>
2026-03-06 20:24:27 -0300

    Rotas criadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,37 @@ export const routes: Routes = [
         (m) => m.DashboardComponent,
       ),
   },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+  },
+  {
+    path: 'pedidos',
+    loadComponent: () =>
+      import('./features/orders/orders.component').then(
+        (m) => m.OrdersComponent,
+      ),
+  },
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import('./features/products/products.component').then(
+        (m) => m.ProductsComponent,
+      ),
+  },
+  {
+    path: 'relatorios',
+    loadComponent: () =>
+      import('./features/reports/reports.component').then(
+        (m) => m.ReportsComponent,
+      ),
+  },
 ];
