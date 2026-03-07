8b9b2700e6964bdc3c7bf98e4916c29d35bf1e7e
Pedro Airton <pedroairton01@gmail.com>
2026-03-07 15:21:46 -0300

    Página de pedidos incrementada

--- src/app/features/orders/orders.component.ts
+++ src/app/features/orders/orders.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatLabel } from '@angular/material/select';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [MatSelectModule, MatFormFieldModule, MatInputModule, MatLabel],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
